f46554ee3aef98cd957cb1e953b8994a83b40abf
Vladimir <vovaykunin@gmail.com>
2023-07-30 09:06:05 +0300

    curs2

---
+++ src/main/java/com/example/cursovaya2/Cursovaya2Application.java
@@ -0,0 +1,13 @@
+package com.example.cursovaya2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cursovaya2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Cursovaya2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/cursovaya2/Cursovaya2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cursovaya2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cursovaya2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
